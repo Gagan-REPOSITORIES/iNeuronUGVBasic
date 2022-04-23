4e7741a5c98c6b6a1de11cea8fffa250924d0bbb
Gagan Deepak R <57809526+Gagan-REPOSITORIES@users.noreply.github.com>
2022-04-23 12:18:23 +0530

    Updated pin and program to actively respond to the keypress

--- RC_Car_ControlTest.py
+++ RC_Car_ControlTest.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-import curses
 import RPi.GPIO as GPIO
-import time
+import keyboard
 
-movement_forward = 6
-movement_backward = 13
+#pin assignment with motor driver
+movement_forward = 13
+movement_backward = 6
 steering_right = 19
 steering_left = 26
 
+#keyboard button assignment
+up = "up"
+down = "down"
+left = "left"
+right = "right"
+p_quit = "q"
 
+#initial keypress will be false
+keypress = False
+
+#GPIO as output
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(movement_forward,GPIO.OUT)
 GPIO.setup(movement_backward,GPIO.OUT)
@@ -18,52 +28,51 @@ GPIO.setup(steering_left,GPIO.OUT)
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
-screen = curses.initscr()
-curses.noecho() 
-curses.cbreak()
-screen.keypad(True)
+
 
 try:
-        while True:   
-            char = screen.getch()
-            if char == ord('q'):
-                break
-            elif char == curses.KEY_UP:
-                keypress_time = int(time.perf_counter())
+        while True:
+
+            if keyboard.is_pressed(up) and not keypress:
+                print("forward")
                 GPIO.output(movement_forward,True)
                 GPIO.output(movement_backward,False)
+                keypress = True
                 
-            elif char == curses.KEY_DOWN:
-                keypress_time = int(time.perf_counter())
+            elif keyboard.is_pressed(down) and not keypress:
+                print("backward")
                 GPIO.output(movement_forward,False)
                 GPIO.output(movement_backward,True)
+                keypress = True
                 
-            elif char == curses.KEY_RIGHT:
-                keypress_time = int(time.perf_counter())
+            elif keyboard.is_pressed(right) and not keypress:
+                print("right")
                 GPIO.output(steering_right,True)
                 GPIO.output(steering_left,False)
+                keypress = True
                 
-            elif char == curses.KEY_LEFT:
-                keypress_time = int(time.perf_counter())
+            elif keyboard.is_pressed(left) and not keypress:
+                print("left")
                 GPIO.output(steering_right,False)
                 GPIO.output(steering_left,True)
+                keypress = True
                 
-            elif char == ord('s'):
+            if keypress and not (keyboard.is_pressed(up) or keyboard.is_pressed(down) or  keyboard.is_pressed(left) or  keyboard.is_pressed(right)):
+                print("stop")
                 GPIO.output(steering_right,False)
                 GPIO.output(steering_left,False)
                 GPIO.output(movement_forward,False)
                 GPIO.output(movement_backward,False)
+                keypress = False
             
-            if((int(time.perf_counter())-keypress_time)>2):
-                char = "stop"
+            if keyboard.is_pressed(p_quit):
+                print("program Exit")
                 GPIO.output(steering_right,False)
                 GPIO.output(steering_left,False)
                 GPIO.output(movement_forward,False)
                 GPIO.output(movement_backward,False)
+                break
 
             
 finally:
-    #Close down curses properly, inc turn echo back on!
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
     GPIO.cleanup()
